43b5de6d450f7efafedabcb1b328fd9a7e8bac3f
Felipe <felpsmarceli@gmail.com>
2026-09-11 14:33:54 -0300

    feat: Criando o program.cs e seu conteudo, para rodar o projeto

--- SistemaLogistica/Program.cs
+++ SistemaLogistica/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SistemaLogistica.Models;
+
+Console.WriteLine("=== SISTEMA DE LOGÍSTICA ===");
+Console.WriteLine();
+
+MotoristaCarreta motorista = new MotoristaCarreta(
+    "Carlos Silva",
+    "FUN001",
+    "ABC1D23"
+);
+
+EntregadorMoto entregador = new EntregadorMoto(
+    "Ana Souza",
+    "FUN002",
+    "A"
+);
+
+List<FuncionarioTransporte> funcionarios = new List<FuncionarioTransporte>
+{
+    motorista,
+    entregador
+};
+
+foreach (FuncionarioTransporte funcionario in funcionarios)
+{
+    funcionario.MostrarDetalhes();
+    Console.WriteLine();
+}
+
+Console.WriteLine("Pressione ENTER para encerrar.");
+Console.ReadLine();
