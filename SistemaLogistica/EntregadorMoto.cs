dc5f2883375c23cb719fb2232ab97a73da293ea1
kauarodrigues1 <rodrigueskaua76@gmail.com>
2026-09-11 10:23:06 -0300

    feat:  Entregadormoto adicionado

---
+++ SistemaLogistica/EntregadorMoto.cs
@@ -0,0 +1,20 @@
+﻿namespace SistemaLogistica.Models;
+
+public class EntregadorMoto : FuncionarioTransporte
+{
+    public string CategoriaCNH { get; set; }
+
+    public EntregadorMoto(string nome, string registro, string categoriaCNH)
+        : base(nome, registro)
+    {
+        CategoriaCNH = categoriaCNH;
+    }
+
+    public override void MostrarDetalhes()
+    {
+        Console.WriteLine("=== Entregador de Moto ===");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Registro: {Registro}");
+        Console.WriteLine($"Categoria da CNH: {CategoriaCNH}");
+    }
+}
